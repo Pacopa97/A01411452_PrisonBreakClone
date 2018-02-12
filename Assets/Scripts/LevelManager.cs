c7d485995b7e49e665ea3c1fff6aa6b51cbd5eec
Francisco Piñeiro Aceituno <franciscopineiroa@hotmail.com>
2018-02-11 22:56:17 -0600

    First Parcial Exam

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;		// Requiered to switch scenes
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		//Debug.Log ("New Level load: " + name);
+		//	Application.LoadLevel (name);    -- This method was deprecated a long time ago
+		SceneManager.LoadScene (name);
+        Brick.breakableCount = 0;
+    }
+
+	public void EndGame(){
+		//Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+
+    //Added functions to the previous LevelManager script
+    public void LoadNextLevel()
+
+    {
+        Brick.breakableCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BrickDestroyed()
+    {
+      
+        if (Brick.breakableCount <= 0)
+        {
+            
+            LoadNextLevel();
+        }
+    }
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    MusicPlayer instance;
+	// Use this for initialization
+	void Start () {
+        if (instance == null)
+        {
+            instance = new MusicPlayer();
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(instance);
+        }
+	}
+	
+
+}
